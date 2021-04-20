722b31178d459e66e835dd2e47d04616efef3f9c
NIHILncunia <nihil_ncunia@naver.com>
2021-04-20 14:42:26 +0900

    2021 0420 .

--- src/components/PostComponents/MainImage.jsx
+++ src/components/PostComponents/MainImage.jsx
@@ -13,10 +13,16 @@ export const MainImage = ({ src, alt, }) => {
     }
   `;
 
+  console.log(src);
+
   return (
     <>
       <div id='content-main-image' css={style}>
-        <img src={src} alt={alt} />
+        {
+          process.env.NODE_ENV === 'development'
+            ? <img src={`${src}`} alt={alt} />
+            : <img src={`/blog${src}`} alt={alt} />
+        }
       </div>
     </>
   );
